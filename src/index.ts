82e17777e6994b18086172af3398f9baca4e8058
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-06-14 13:15:01 +0200

    Add static web server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import http, { IncomingMessage, ServerResponse } from "http";
+import path from "path";
+import fs from "fs/promises";
+
+async function requestListener(req: IncomingMessage, res: ServerResponse) {
+  const filePath = path.join(__dirname, "static/index.html");
+
+  const data = await fs.readFile(filePath, "utf8");
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": data.length,
+  });
+  res.write(data);
+  res.end();
+}
+
+http.createServer(requestListener).listen(3000, () => {
+  console.log("HTTP Server listening on port 3000...");
+});
